aff5748b790a002a9bb6a11b13942012c01b70a9
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-11-18 09:05:43 +0200

    Clear coach

--- app/(coach)/_layout.tsx
+++ app/(coach)/_layout.tsx
@@ -1,58 +1,16 @@
-import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import { useEffect } from 'react';
-import LoginScreen from '../api/screens/LoginScreen';
-import { useAuthStore } from '../api/store';
-import { ScreenContainer } from '../components/containers/ScreenContainer';
-import { colors } from '../constants/colors';
 
-
-export default function RootLayout() {
-  const { isLogin, restoreAuth } = useAuthStore();
-
-  useEffect(() => {
-    restoreAuth();
-  }, []);
-
-  if (isLogin === false) {
-    return <LoginScreen onSwitch={() => {}} />;
-  }
-
+export default function Layout() {
   return (
-    <ScreenContainer>
-      <Tabs screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: colors.yellow[400],
-          tabBarInactiveTintColor: colors.blue[300],
-          tabBarStyle: {
-            backgroundColor: colors.cyan[900],
-            height: 60,
-            paddingTop: 5,
-            borderTopWidth: 1,
-            borderTopColor: colors.gray[500],
-          },
-          tabBarLabelStyle: {
-            fontSize: 12,
-          },
-        }}
-      >
-        <Tabs.Screen name="chat"
-          options={{
-            title: 'Chat',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="chatbubble-outline" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen name="users"
-          options={{
-            title: 'Users',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="people-outline" size={size} color={color} />
-            ),
-          }}
-        />
-      </Tabs>
-    </ScreenContainer>
+    <Tabs>
+      <Tabs.Screen
+        name="home"
+        options={{ title: 'Главная', headerShown: false }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ title: 'Профиль', headerShown: false }}
+      />
+    </Tabs>
   );
 }
